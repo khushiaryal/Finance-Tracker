e0a81eb4dedd166bf61fa84b8c2de5486dc675f5
Khushi Aryal <aryalkhushi92@gmail.com>
2026-07-18 08:04:59 +0545

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,27 @@
+// Check login status
+if(localStorage.getItem("loggedIn") !== "true"){
+    window.location.href = "index.html";
+}
+
+// Show logged in user's name (optional)
+const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+if(currentUser){
+
+    const username = document.getElementById("username");
+
+    if(username){
+        username.textContent = currentUser.name;
+    }
+
+}
+
+// Logout function
+function logout(){
+
+    localStorage.removeItem("loggedIn");
+    localStorage.removeItem("currentUser");
+
+    window.location.href = "dashboard.html";
+
+}
